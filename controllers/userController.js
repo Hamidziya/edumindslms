f81bd0752d12405d7389496d471ddabffe5ff157
Hamid Ziya <hamidziya123@gmail.com>
2024-10-01 16:38:37 +0530

    user delete dummy api

--- controllers/userController.js
+++ controllers/userController.js
@@ -285,3 +285,24 @@ exports.getUserCourseDummy = async (req, res) => {
     res.status(500).json({ message: "Server error" });
   }
 };
+
+exports.deleteUserCourseDummy = async (req, res) => {
+  const { userid, courseIdToDelete } = req.body;
+
+  try {
+    const user = await User.findByPk(userid);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    let existingCourses = user.courseIds || [];
+    const updatedCourses = existingCourses.filter(
+      (course) => course.courseId !== courseIdToDelete
+    );
+    await user.update({ courseIds: updatedCourses });
+
+    res.status(200).json({ message: "Course removed successfully", user });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
