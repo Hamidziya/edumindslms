a00fadb886d45518eaa5d33133aab9cbd9ae2d5a
Hamid Ziya <hamidziya123@gmail.com>
2024-10-10 11:42:38 +0530

    active course list new api

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -76,3 +76,19 @@ exports.getActiveCourseList = async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 };
+
+exports.getActiveCourseListDummy = async (req, res) => {
+  try {
+    const courses = await Course.findAll({
+      where: { isDelete: false },
+    });
+
+    if (courses.length === 0) {
+      return res.status(404).json({ message: "No active Course found" });
+    }
+
+    res.status(200).json(courses);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
