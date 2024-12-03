dd52b63f03556d8e1a3a2922de20541e857614f4
Hamid Ziya <hamidziya123@gmail.com>
2024-12-03 18:47:25 +0530

    new domain access for blog

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:5173", // Development frontend
   "https://test.eduplaced.com", // Testing frontend
-  "https://study.eduplaced.com", // Production frontend
+  "https://study.eduplaced.com",
+  "https://eduplaced.edumindslearning.com", // Production frontend
 ];
 
 // Enable CORS for specific origins
