ec014df304ffa3e127cd0074b5c8f70ff1fa8d30
Hamid Ziya <hamidziya123@gmail.com>
2025-01-24 15:13:45 +0530

    new comments in user pis

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -160,10 +160,6 @@ exports.deleteCourseSection = async (req, res) => {
       return res.status(404).json({ message: "Course section not found" });
     }
 
-    // if (!course) {
-    //   return res.status(404).json({ message: "Course section not found" });
-    // }
-
     await course.update({
       isDelete: true,
     });
