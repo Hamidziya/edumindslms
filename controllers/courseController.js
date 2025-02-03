fc4c63cd8c4a3f4f3cc1bf0b5bc522e2ba3ef707
Hamid Ziya <hamidziya123@gmail.com>
2025-02-03 14:01:01 +0530

    new commented code

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -90,6 +90,10 @@ exports.getActiveCourseList = async (req, res) => {
       return res.status(404).json({ message: "No active Course found" });
     }
 
+    // if (courses.length === 0) {
+    //   return res.status(404).json({ message: "No active Course found" });
+    // }
+
     res.status(200).json({
       message: "Course List",
       status: "success",
