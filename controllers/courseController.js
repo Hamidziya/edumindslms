a8f92d9b6ec35347d3a00b219e81e44938eea7f1
Hamid Ziya <hamidziya123@gmail.com>
2025-02-05 13:58:42 +0530

    commented code sorted

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -181,9 +181,6 @@ exports.getActiveCourseSectionList = async (req, res) => {
       return res.status(404).json({ message: "No active Course found" });
     }
 
-    // if (courses.length === 0) {
-    //   return res.status(404).json({ message: "No active Course found" });
-    // }
     res.status(200).json({
       message: "Course Section List",
       status: "success",
