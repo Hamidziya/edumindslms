06177192179c1adfe9a7dfdfaededcc005739674
Hamid Ziya <hamidziya123@gmail.com>
2025-02-06 19:47:00 +0530

    student create fro university

--- controllers/uniController.js
+++ controllers/uniController.js
@@ -28,26 +28,26 @@ exports.uniRegister = async (req, res) => {
   }
 };
 
-// exports.createStudent = async (req, res) => {
-//   const tosave = req.body.data;
-//   try {
-//     const users = await university.findAll({
-//       where: {
-//         email: tosave.email,
-//       },
-//     });
-//     if (users.length > 0) {
-//       return res.status(201).json({ message: "User Already Exist" });
-//     }
+exports.createStudent = async (req, res) => {
+  const tosave = req.body.data;
+  try {
+    const users = await university.findAll({
+      where: {
+        email: tosave.email,
+      },
+    });
+    if (users.length > 0) {
+      return res.status(201).json({ message: "User Already Exist" });
+    }
 
-//     const newUser = await university.create(tosave);
-//     res
-//       .status(201)
-//       .json({ message: "Student Created", status: "success", data: newUser });
-//   } catch (err) {
-//     res.status(500).json({ error: err.message });
-//   }
-// };
+    const newUser = await university.create(tosave);
+    res
+      .status(201)
+      .json({ message: "Student Created", status: "success", data: newUser });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
 // exports.getRegistratedUsers = async (req, res) => {
 //   try {
 //     const users = await User.findAll({
