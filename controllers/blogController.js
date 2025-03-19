1204644e810c5e4a7915bc46f9e15dc56032a4de
Hamid Ziya <hamidziya123@gmail.com>
2025-03-19 16:16:56 +0530

    new condition

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -175,9 +175,10 @@ exports.getBlogListEduplaced = async (req, res) => {
 
     if (!blogs || blogs.length === 0) {
       return res.status(404).json({ message: "Blog Not Found" });
-    } else {
-      console.log("New logging for the blog list");
     }
+    // } else {
+    //   console.log("New logging for the blog list");
+    // }
 
     res.status(200).json({
       message: "Blog List",
