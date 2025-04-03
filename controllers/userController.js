fb442a77943f76295e09dbbb95b0744068660652
Hamid Ziya <hamidziya123@gmail.com>
2025-04-03 20:02:20 +0530

    new logging to change the user list

--- controllers/userController.js
+++ controllers/userController.js
@@ -103,6 +103,13 @@ exports.getUsers = async (req, res) => {
       return res.status(404).json({ message: "User not found" });
     }
 
+    // else{
+    //   if(users){
+    //     console.log("logger in user list")
+    //     res.status()
+    //   }
+    // }
+
     res.status(200).json({
       message: "User List",
       status: "success",
